5da3a79bb73b9e14d99c0dd5cbaa14f53378e2d5
josebr67 <jlbru89@gmail.com>
2021-05-07 22:41:54 -0300

    update heroku 7

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL && 'http://localhost.com:8080/';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL && 'https://localhost.com:8080/';
 //conexão heroku.
